015d76a49e55e75689ba8fcb6a58ce9d4b841e7b
Koen van Haren <koen-h@hotmail.com>
2019-06-17 11:43:38 +0200

    Added CustomMiddleware, and the CustomMiddlewareExtentions handler Registered CustomMiddleware sample in Startup.cs

---
+++ CoreMiddlewareSample.web/CoreMiddlewareSample.web/Middleware/CustomMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMiddlewareSample.web.Middleware
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomMiddleware(RequestDelegate _next) //, and other dependencies
+        {
+            this._next = _next;
+        }
+        public async Task Invoke(HttpContext context) //, and other dependencies)
+        {
+            //Middleware logic
+            if (!context.Request.Headers.Values.Contains(""))
+            {
+                //logic
+            }
+            
+            await _next.Invoke(context); //invoke next middleware (Required!)
+
+            //Code executed after the next middleware
+
+        }
+    }
+}
---
+++ CoreMiddlewareSample.web/CoreMiddlewareSample.web/Middleware/CustomMiddlewareExtentions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMiddlewareSample.web.Middleware
+{
+    public static class CustomMiddlewareExtentions
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<CustomMiddleware>();
+            return app;
+        }
+    }
+}
